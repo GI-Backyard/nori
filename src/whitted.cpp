3529da38726b77b681cababdbf31c85569fff54a
huabingxu <huabingxu@tencent.com>
2020-06-07 03:15:30 +0800

    distributed shading works

--- src/whitted.cpp
+++ src/whitted.cpp
@@ -1,6 +1,9 @@
 #include <nori/integrator.h>
 #include <nori/scene.h>
 #include <algorithm>
+#include <nori/emitter.h>
+#include <nori/warp.h>
+#include <nori/bsdf.h>
 
 NORI_NAMESPACE_BEGIN
 
@@ -12,10 +15,48 @@ public:
     Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray) const {
         Intersection its;
         if(!scene->rayIntersect(ray, its)) {
-            return Color3f(0.0, 0.0, 0.0);
+            return Color3f(0.1, 0.1, 0.1);
         }
-        Normal3f n = its.shFrame.n.cwiseAbs();
-        return Color3f(n.x(), n.y(), n.z());
+        
+        Color3f Ltotal(0, 0, 0);
+        // Le
+        if(its.mesh->getEmitter())
+        {
+            EmitterQueryRecord record;
+            Ltotal = its.mesh->getEmitter()->eval(record);
+        }
+
+        // sampling Light
+        std::vector<Emitter*> lights;
+        for(auto& mesh : scene->getMeshes())
+        {
+            if(mesh->getEmitter()) lights.push_back(mesh->getEmitter());
+        }
+        uint32_t sampledLight = sampler->next1D() * lights.size();
+        EmitterQueryRecord lightRecord;
+        Color3f Le = lights[sampledLight]->sample(lightRecord, Point3f(sampler->next1D(), sampler->next1D(), sampler->next1D()));
+        float pdf = lights[sampledLight]->pdf(lightRecord);
+        pdf = pdf / lights.size();
+        
+        Vector3f wo = -ray.d;
+        wo.normalize();
+        Vector3f wi = lightRecord.position - its.p;
+        float lengthwi = wi.norm();
+        wi.normalize();
+        
+        Ray3f secondaRay(its.p, wi);
+        secondaRay.maxt = lengthwi - Epsilon;
+        if(!scene->rayIntersect(secondaRay))
+        {
+            float geom = its.shFrame.n.dot(wi) * lightRecord.normal.dot(-wi) / (lengthwi * lengthwi);
+            if(geom < 0) geom = 0;
+            // bsdf
+            Color3f reflection = its.mesh->getBSDF()->eval(BSDFQueryRecord(its.toLocal(wi), its.toLocal(wo), EMeasure::ESolidAngle));
+            Ltotal += Le * reflection * geom / pdf;
+        }
+
+
+        return Ltotal;
     }
     /// Return a human-readable description for debugging purposes
     std::string toString() const {
